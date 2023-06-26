ee2c443635bebee157f5d6aadc7aa17de6ba83f6
brvn0 <me@brvn0.de>
2023-06-26 19:06:13 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ cd /go
 
 WRITEFREELY=cmd/writefreely/writefreely
 
-if [ ! -e ./keys/email.aes256 ]; then
+if [ ! -e keys/email.aes256 ]; then
+echo "Generating keys..."
 "${WRITEFREELY}" generate keys && echo "Keys generated!"
 "${WRITEFREELY}" db init && echo "Database initialized!"   
 fi
